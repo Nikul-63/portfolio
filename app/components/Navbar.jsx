a5405edec95e2add07215555de16013f0f184934
Nikul Dabhi <dabhinikul625@gmail.com>
2026-06-09 08:48:21 +0530

    fix : fixing navbar issue on smaller devices..!

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -58,8 +58,8 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
                 </div>
 
                 {/* mobile menu */}
-                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-dark-theme'>
-                    <div className='absolute right-6 top-6' onClick={closeMenu}>
+                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen  bg-rose-50 transition duration-500 dark:bg-dark-theme'>
+                    <div className='absolute right-6 top-6 cursor-pointer' onClick={closeMenu}>
                         <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5' />
                     </div>
 
